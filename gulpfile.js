6113d4e49cd86b11a9619250f19a9a1691a81b8d
Jacob Daitzman <Jacob@kitchen-imac.local>
2015-12-04 17:25:37 -0500

    init commit with gulpfile progress

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp');
+var notify = require('gulp-notify');
+var gls = require('gulp-live-server');
+var uglify = require('gulp-uglify');
+var uglifycss = require('gulp-uglifycss');
+var sourcemaps = require('gulp-sourcemaps');
+var jshint = require('gulp-jshint');
+var stylish = require('gulp-jscs-stylish');
+var concat = require('gulp-concat');
+
+gulp.task('js', function() {
+
+});
+
+gulp.task('css', function() {
+
+});
+
+gulp.task('watch', function() {
+
+});
