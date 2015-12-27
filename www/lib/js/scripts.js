2d610c5784ab26427eae272c7ea6c6ffdb455c27
jdtzmn <jdtzmn@gmail.com>
2015-12-27 16:06:57 -0500

    trying to support gulp-include

---
+++ www/lib/js/scripts.js
@@ -0,0 +1,8 @@
+/*=include ../bower_components/auth0-lock-passwordless/build/lock-passwordless.min.js */
+/*=include ../bower_components/jquery/dist/jquery.min.js */
+/*=include ../bower_components/bootstrap/dist/js/bootstrap.min.js */
+/*=include ../bower_components/transparency/dist/transparency.min.js */
+/*=include ../bower_components/pnotify/dist/pnotify.min.js */
+/*=include ../bower_components/pnotify/dist/pnotify.desktop.min.js */
+/*=include ../bower_components/pnotify/dist/pnotify.mobile.min.js */
+/*=include ../css/**\/!(scripts.js) */
